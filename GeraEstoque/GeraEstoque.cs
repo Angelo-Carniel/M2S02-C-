083c2df4e826efcf289e417dcfdca480828c0d6a
Angelo Carniel <angelofilho.e.t.d@gmail.com>
2022-06-09 18:57:35 -0300

    Atividade 06

--- GeraEstoque/GeraEstoque.cs
+++ GeraEstoque/GeraEstoque.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine(@"
+Seja bem vindo ao GeraEstoque 1.0
+---------------------------------
+1 Cadastrar produto
+2 Consultar produto
+3 Modificar produto
+4 Excluir produto
+0 Sair");
         
+        Console.WriteLine("Digite a opção:");
+        Console.ReadLine();
     }
 }
