e5fd472a82854111f21179be79e1e04b45551197
Angelo Carniel <angelofilho.e.t.d@gmail.com>
2022-06-10 18:24:10 -0300

    Atividade07 método CadastrarProduto implementado

--- GeraEstoque/GeraEstoque.cs
+++ GeraEstoque/GeraEstoque.cs
@@ -4,16 +4,75 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(@"
-Seja bem vindo ao GeraEstoque 1.0
----------------------------------
-1 Cadastrar produto
-2 Consultar produto
-3 Modificar produto
-4 Excluir produto
-0 Sair");
-        
-        Console.WriteLine("Digite a opção:");
-        Console.ReadLine();
+        Console.Clear();
+        ShowOptions();
+        var option = short.Parse(Console.ReadLine());
+
+        switch(option)
+        {
+            case 1: CadastrarProduto(); break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+            case 0:
+            {
+                Console.Clear();
+            } break;
+        }
+    }
+
+    static void ShowOptions()
+    {
+        Console.SetCursorPosition(1,2);
+        Console.WriteLine("Seja bem vindo ao GeraEstoque v0.1");
+        Console.SetCursorPosition(1,3);
+        Console.WriteLine("===================================");
+
+        Console.SetCursorPosition(1,4);
+        Console.WriteLine("1 - Cadastrar produto");
+        Console.SetCursorPosition(1,5);
+        Console.WriteLine("2 - Consultar produto");
+        Console.SetCursorPosition(1,6);
+        Console.WriteLine("3 - Modificar produto");
+        Console.SetCursorPosition(1,7);
+        Console.WriteLine("4 - Excluir produto");
+        Console.SetCursorPosition(1,8);
+        Console.WriteLine("0 - Sair");
+
+        Console.SetCursorPosition(1,9);
+        Console.WriteLine("Opção selecionada");
+        Console.SetCursorPosition(1,9);
+        Console.WriteLine("Opção selecionada: ");
+    }
+
+    static void CadastrarProduto()
+    {
+        string descricao;
+        double quantidade;
+        double valor;
+        char continua = 's';
+
+        do{
+
+            Console.WriteLine("___Comanda___");
+
+            Console.WriteLine("Insira a descrição do produto:");
+            descricao = Console.ReadLine();
+            
+            Console.WriteLine("Insira a quantidade:");
+            quantidade = double.Parse(Console.ReadLine()); 
+            
+            Console.WriteLine("insira o valor:");
+            valor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Deseja continuar? (s/n)");
+            continua = Convert.ToChar(Console.ReadLine());
+
+            //p0.addItem(descricao, quantidade, valor);
+
+            }while (continua == 's');
+
+            //Console.WriteLine("Total: " + p0.TotalPedido())
+
     }
 }
